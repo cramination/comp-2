3bd81bc7c1d3204e5e6e4cae29cf5f467c7f0163
cramination <camnation@comcast.net>
2025-01-25 15:24:55 -0500

    first commit

---
+++ public/main.js
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+
+
+// scene
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const light = new THREE.AmbientLight( 0x404040, 25 ); // soft white light
+scene.add( light );
+
+const light2 = new THREE.PointLight( 0xFFC0CB, 2, 100 );
+light2.position.set( -6.5, 0, 6 );
+scene.add( light2 );
+
+const light3 = new THREE.PointLight( 0xFFC0CB, 1.5, 100 );
+light3.position.set( 1, 1, 3);
+scene.add( light3 );
+
+
+// render
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.target.set(1, .5, 3.6)
+controls.maxDistance = 4;
+controls.enablePan = false
+controls.minDistance = .5
+
+camera.position.set(3, .5, 6)
+
+function onWindowResize() {
+  // Get the new window dimensions
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+renderer.setSize( width, height );
+
+camera.aspect = width / height;
+
+camera.updateProjectionMatrix();
+}
+
+window.addEventListener('resize', onWindowResize);
+onWindowResize();
+
+// objects
+
+let comp ;
+
+
+loader.load(
+	"/assets/Computer-case-compressed.glb",
+	function ( gltf ) {
+    comp = gltf.scene
+    comp.position.set(-.6, 0, 3.6)
+    comp.scale.set(2, 2, 2)
+		scene.add( gltf.scene )
+    gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+	},
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}, 
+(error) => {
+    console.error('An error happened', error);
+}
+);  
+
+
+// animate
+
+function animate() {
+    requestAnimationFrame( animate );
+	controls.update();
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
