4504a62eb23ef96d9d21d18ff17d19ea323980ac
cramination <camnation@comcast.net>
2025-01-30 19:10:08 -0500

    vite..config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
  
  export default defineConfig({
-    base: "/comp/",
+    base: "/comp-2/",
     server: {
 
         port: 5137 // Change this to your desired port
